61f7397195623f48adbe3dca832986c7776911a6
AnckerStudios <horohi26@gmail.com>
2024-12-23 12:47:57 +0400

    init

---
+++ src/main/java/com/example/bakeryeureka/BakeryEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.example.bakeryeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class BakeryEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakeryEurekaApplication.class, args);
+    }
+
+}
